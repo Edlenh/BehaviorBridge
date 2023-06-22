81a1853e5e3cabed5da14c8977c6277cdf430e43
Edward <herreraed93@gmail.com>
2023-06-22 10:49:55 -0700

    start calender widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,18 @@ import {useState} from "react";
 import {Container , Grid} from "@mui/material"
 import UserHeader from "@/components/User/Header";
 import Calender from "@/components/Calender";
+import { Event } from "@/types";
 
 const Home = () => {
+  const [events,setEvents]=useState<Array<Event>>([])
+  const [open, setOpen] =useState(false)
   return ( 
     <Container>
       <Grid>
         <UserHeader />
       </Grid>
       <Grid>
-        <Calender />
+        <Calender events={events} setOpen={setOpen}/>
       </Grid>
     </Container>
    );
--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -3,10 +3,57 @@
 import dayjs from "dayjs"
 import CalendarHeatmap from 'react-calendar-heatmap';
 import "react-calendar-heatmap/dist/styles.css";
+import {Alert, AlertTitle, Button, Grid} from "@mui/material";
+import { Event } from "@/types";
 
-export default function Calender(){
+type Props ={
+    events: Event[];
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+};
+
+//pass props to the calender
+export default function Calender({events, setOpen}: Props){
     let yearly = dayjs().subtract(365, "days").format("YYYY-MM-DD");
-    return<CalendarHeatmap startDate ={yearly} values={[]}/>
-}
+    const formattedEvents = events.map((event)=>({
+        ...event,
+        //spread through all events
+        date: dayjs(event.date).format("YYYY-MM-DD"),
+    }))
+    return(
+        <Grid item className="Calender border" sx={{p:4}}>
+            <Grid 
+            direction="row"
+            container item
+            justifyContent="space-between"
+            alignItems="center"
+            justifyItems="center"
+            sx={{mb:4}}>
+                <h1>Track New Behavior</h1>
+                <Button
+                className="info"
+                variant="outlined"
+                onClick={()=>setOpen(true)}
+                >
+                    New
+                </Button>
+            </Grid>
+            <CalendarHeatmap startDate ={yearly}
+            showWeekdayLabels
+            values={formattedEvents}
+            classForValue={(value)=>{
+                if(!value){
+                    return"color-empty";
+                }
+                return `primary opacity-${value.count}`;
+            }}
+            />
+            {/* todo: add chatgpt engine to provide recs */}
+            <Alert severity="info">
+                <AlertTitle>Aba</AlertTitle>
+                <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati</span>
+                </Alert> 
+        </Grid>
+    );
+};
   
     
